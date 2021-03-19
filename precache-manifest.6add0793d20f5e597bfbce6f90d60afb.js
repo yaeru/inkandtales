349fd5171a23f52e6684f729b46d2ab27f521e5f
Yael <yaelduckwen@gmail.com>
2021-03-18 21:09:36 -0300

    test pwa

---
+++ precache-manifest.6add0793d20f5e597bfbce6f90d60afb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde95c87fca4053a4564",
+    "url": "/css/app.7fe4b2dc.css"
+  },
+  {
+    "revision": "c1b4fae46a264e7ff29fc0480055fbf7",
+    "url": "/custommp.js"
+  },
+  {
+    "revision": "5c56b127db1c854a13cc338bd82eebeb",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "bb2ebe4406f684f010943c0df10d60ca",
+    "url": "/favicon/icon-192x192.png"
+  },
+  {
+    "revision": "02f11eb075d00465c8934723dd414faa",
+    "url": "/favicon/icon-256x256.png"
+  },
+  {
+    "revision": "73eabde21dcd5670ed2e3cb23ca6d955",
+    "url": "/favicon/icon-384x384.png"
+  },
+  {
+    "revision": "eab7ce707c4d7f9d6af9b5a80c286566",
+    "url": "/favicon/icon-512x512.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/favicon/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9c07b586890be8304f4316d0a593c73",
+    "url": "/img/logo-inverse.b9c07b58.png"
+  },
+  {
+    "revision": "7c5d0bd583f5c49df3e433b871776ae8",
+    "url": "/img/samay.7c5d0bd5.jpg"
+  },
+  {
+    "revision": "7369f14934335f61cab1198883293b6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fde95c87fca4053a4564",
+    "url": "/js/app.5b776a6d.js"
+  },
+  {
+    "revision": "d3f9f2c52b8323c1539b",
+    "url": "/js/chunk-vendors.fc75d6a2.js"
+  },
+  {
+    "revision": "156f46cdfdced86ced15ffe7a8194057",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
